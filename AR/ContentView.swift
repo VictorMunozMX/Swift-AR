720224f0b2cd9e3628d37dbb3629f63f061fec7e
VictorMunozMX <kkienrique@hotmail.com>
2024-03-22 14:51:07 -0700

    Lab8 - complete

--- AR/ContentView.swift
+++ AR/ContentView.swift
@@ -11,20 +11,28 @@ var worldAnchor: Experience.World?
 
 struct ContentView : View {
     
+    @State var counter: Int = 0
+    
     var body: some View {
         VStack{
             //return ARViewContainer().edgesIgnoringSafeArea(.all)
-            ARViewContainer().edgesIgnoringSafeArea(.all)
+            ARViewContainer(counter: $counter).edgesIgnoringSafeArea(.all)
             
             Button(action: {
                 worldAnchor?.notifications.redChair.post()}, label: {
                     Text("Button")
-                })
+                }
+            )
+            Text("Count: \(counter)")
+                .font(.system(size: 20))
         }
     }
-}
+}	
+
 
 struct ARViewContainer: UIViewRepresentable {
+    
+    @Binding var counter: Int
     
     func makeUIView(context: Context) -> ARView {
         
@@ -33,7 +41,9 @@ struct ARViewContainer: UIViewRepresentable {
         // Load the "Box" scene from the "Experience" Reality File
         worldAnchor = try! Experience.loadWorld()
         
-        worldAnchor!.actions.planeWasHit.onAction = { entity in print("Plane was Hit") }
+        worldAnchor?.actions.planeWasHit.onAction = { entity in print("Plane was Hit")
+            counter += 1
+        }
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(worldAnchor!)
